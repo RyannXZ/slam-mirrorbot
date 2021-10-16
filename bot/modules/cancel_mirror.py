485381429fadab90be660fb11954fd3650e6cd75
RyannXZ <92634548+RyannXZ@users.noreply.github.com>
2021-10-16 23:27:03 +0800

    Update cancel_mirror.py

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -16,7 +16,7 @@ def cancel_mirror(update, context):
         gid = args[1]
         dl = getDownloadByGid(gid)
         if not dl:
-            sendMessage(f"GID: <code>{gid}</code> Not Found.", context.bot, update)
+            sendMessage(f"GID: <code>{gid}</code> Tidak ditemukan.", context.bot, update)
             return
         mirror_message = dl.message
     elif update.message.reply_to_message:
@@ -33,7 +33,7 @@ def cancel_mirror(update, context):
             if BotCommands.MirrorCommand in mirror_message.text or \
                BotCommands.TarMirrorCommand in mirror_message.text or \
                BotCommands.UnzipMirrorCommand in mirror_message.text:
-                msg1 = "Mirror Already Have Been Cancelled"
+                msg1 = "Sudah dibatalkan njir"
                 sendMessage(msg1, context.bot, update)
             else:
                 sendMessage(msg, context.bot, update)
@@ -42,11 +42,11 @@ def cancel_mirror(update, context):
             sendMessage(msg, context.bot, update)
             return
     if dl.status() == MirrorStatus.STATUS_ARCHIVING:
-        sendMessage("Archival in Progress, You Can't Cancel It.", context.bot, update)
+        sendMessage("Arsip sedang diproses, Gabisa dibatalin anjay.", context.bot, update)
     elif dl.status() == MirrorStatus.STATUS_EXTRACTING:
-        sendMessage("Extract in Progress, You Can't Cancel It.", context.bot, update)
+        sendMessage("Ekstrak sedang diproses, Gabisa dibatalin anjay.", context.bot, update)
     elif dl.status() == MirrorStatus.STATUS_SPLITTING:
-        sendMessage("Split in Progress, You Can't Cancel It.", context.bot, update)
+        sendMessage("Split in Progress, Gabisa dibatalin anjay.", context.bot, update)
     else:
         dl.download().cancel_download()
         sleep(3)  # incase of any error with ondownloaderror listener
@@ -66,7 +66,7 @@ def cancel_all(update, context):
                 sleep(0.3)
         else:
             break
-    sendMessage(f'{count} Download(s) has been Cancelled!', context.bot, update)
+    sendMessage(f'{count} Unduhan(s) Dibatalkan 🦋', context.bot, update)
 
 
 
